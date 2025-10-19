24f1ac1b8bbfa8c19a437388a65db5204beb4ebb
Viktor <vikkes@gmail.com>
2025-10-19 14:33:58 +0200

    fix

--- src/composables/useIcebergDuckDB.ts
+++ src/composables/useIcebergDuckDB.ts
@@ -38,7 +38,7 @@ export function useIcebergDuckDB() {
 
       // Attach the Iceberg catalog
       const attachQuery = `
-        ATTACH 'warehouse' AS ${config.catalogName} (
+        ATTACH '${config.catalogName}' AS ${config.catalogName} (
           TYPE iceberg,
           SECRET iceberg_secret,
           ENDPOINT '${config.restUri}'
