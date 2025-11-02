c6d621914e0573beca652de588aeb454331c5a77
Viktor <vikkes@gmail.com>
2025-11-02 19:04:21 +0100

    fix

--- src/composables/useStorageValidation.ts
+++ src/composables/useStorageValidation.ts
@@ -17,7 +17,7 @@ export function useStorageValidation(
   const supportedStorageTypes = ['s3']; //, 'gcs'
 
   // List of supported protocols for DuckDB WASM
-  const supportedProtocols = ['https:', 'http:'];
+  const supportedProtocols = ['https:']; // 'http:' disabled since duckdb is not supporting it well
 
   /**
    * Check if the storage type is supported by DuckDB WASM
