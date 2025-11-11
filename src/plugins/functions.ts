84b6381df4cf46bff46c01b696803dbad9e72d87
Viktor <vikkes@gmail.com>
2025-11-11 09:10:42 +0100

    debug

--- src/plugins/functions.ts
+++ src/plugins/functions.ts
@@ -216,6 +216,7 @@ function setError(error: any, ttl: number, functionCaused: string, type: Type, n
 
       // Add to notification store for navbar notifications
       console.log('Notifying error error.stack:', error.error.stack);
+      console.log('Notifying error error.stack:', error.error.stack ? error.error.stack : []);
       if (notify) {
         notificationStore.addNotification({
           function: functionCaused,
